d915403ac5c8f221e3395a818799f95c5a479ea0
Joao Pedro <joaoppuccini@gmail.com>
2020-07-09 16:47:40 -0300

    Colocando MongoDB container

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
     //module.exports = {mongoURI: "mongodb+srv://joaopuccini:0409@cluster0-jefvh.mongodb.net/blogapp?retryWrites=true&w=majority"}
-    module.exports = {mongoURI: "image-registry.openshift-image-registry.svc:5000/openshift/mongodb@sha256:c260fda42b5113d0183f85d7c9e873101268d91f1e74dcb38cfe76269b4ed47a"}
+    module.exports = {mongoURI: "mongodb://admin:admin@172.30.68.212:27017/projects"}
     
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
